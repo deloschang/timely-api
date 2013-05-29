d301ce9a79a2ace50a4e74d7605ed02d3aa74010
Delos Chang <delosfchang@gmail.com>
2013-05-29 03:21:12 -0400

    updating without postgis database

--- timelyapi/settings.py
+++ timelyapi/settings.py
@@ -14,8 +14,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'timelydb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
--- webapp/models.py
+++ webapp/models.py
@@ -1,6 +1,1 @@
-from django.contrib.gis.db import models
 
-class Polygon(models.Model):
-    name = models.CharField(max_length=16)
-    poly = models.PolygonField()
-    objects = models.GeoManager()
